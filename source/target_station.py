9e0ad6775cde5af3923563663f1e6cd0b060748a
Steve Lilley <py1sl@users.noreply.github.com>
2025-01-22 21:22:57 +0000

    Update target_station.py

--- source/target_station.py
+++ source/target_station.py
@@ -9,3 +9,7 @@ class target_station(facility_system):
     def __init__(self):
         self.instrument_list = []
         self.moderator_list = []
+        self.num_instruments = None
+        self.target_type = None
+        self.num_moderators = None
+        self.epb_length = None
